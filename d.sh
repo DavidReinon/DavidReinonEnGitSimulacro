35c022d7e0113edd9a225a53730953f878cab05e
darega <davidreinongarcia@gmail.com>
2023-02-14 00:57:31 +0100

    d.sh

---
+++ d.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ $1 -ge 0 -a $1 -le 10 ]; then
+    if [ $1 -ge 0 -a $1 -lt 5 ]; then
+        echo "La nota es un suspenso."
+    elif [ $1 -ge 5 -a $1 -le 8 ]; then
+        echo "La nota es un aprobado."
+    else
+        echo "La nota es un sobresalinete."
+    fi
+else
+    echo "la nota suministrada debe de estar entre el 0 y el 10"
+fi
